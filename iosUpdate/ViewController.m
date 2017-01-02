42efa95c4eea65017f98e4b8a3c6423943f7c1af
jiumogaoao <gaozhiqiang@gaozhiqiangdeMac.local>
2017-01-02 00:22:16 -0800

    change
    Change

--- iosUpdate/ViewController.m
+++ iosUpdate/ViewController.m
@@ -1,13 +1,13 @@
 //
 //  ViewController.m
-//  iosUpdate
+//  update
 //
-//  Created by 高志强 on 2017/1/1.
-//  Copyright © 2017年 高志强. All rights reserved.
+//  Created by 高志强 on 2016/12/29.
+//  Copyright © 2016年 高志强. All rights reserved.
 //
 
 #import "ViewController.h"
-
+#import "ZipArchive.h"
 @interface ViewController ()
 
 @end
@@ -17,6 +17,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    ZipArchive* zip = [[ZipArchive alloc] init];
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *dcoumentpath = ([paths count] > 0) ? [paths objectAtIndex:0] : nil;
+    NSLog(@"%@", dcoumentpath);
+    NSString* l_zipfile = [dcoumentpath stringByAppendingString:@"/2.zip"] ;
+    NSString* unzipto = [dcoumentpath stringByAppendingString:@"/test"] ;
+    if( [zip UnzipOpenFile:l_zipfile] ) {
+        BOOL ret = [zip UnzipFileTo:unzipto overWrite:YES];
+        if( NO==ret ) {
+            NSLog(@"没解压");
+        }else{
+            NSLog(@"已解压");
+        }
+        [zip UnzipCloseFile];
+    }
+    //  [zip release];
 }
 
 
