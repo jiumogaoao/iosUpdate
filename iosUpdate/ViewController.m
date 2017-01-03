7265a36afadb7dd80d7d0b46a7099ea92033c34c
jiumogaoao <gaozhiqiang@gaozhiqiangdeMac.local>
2017-01-03 14:53:52 +0800

    change
    Change

--- iosUpdate/ViewController.m
+++ iosUpdate/ViewController.m
@@ -39,7 +39,7 @@
     //1.创建管理者对象
     AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
     //2.确定请求的URL地址
-    NSURL *url = [NSURL URLWithString:@"http://192.168.1.102/down/2.zip"];
+    NSURL *url = [NSURL URLWithString:@"http://192.168.1.139/down/2.zip"];
     
     //3.创建请求对象
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
@@ -50,16 +50,35 @@
         NSLog(@"%lf",1.0 * downloadProgress.completedUnitCount / downloadProgress.totalUnitCount);
         
     } destination:^NSURL * _Nonnull(NSURL * _Nonnull targetPath, NSURLResponse * _Nonnull response) {
-        //下载地址
-        NSString *cachesPath = [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) lastObject];
-        NSString *path = [cachesPath stringByAppendingPathComponent:response.suggestedFilename];
-        NSLog(@"下载地址:%@",path);
-        return [NSURL URLWithString:path];
+        // 告诉服务器下载的文本保存的位置在那里
+        NSURL *documentsDirectoryURL = [[NSFileManager defaultManager] URLForDirectory:NSDocumentDirectory inDomain:NSUserDomainMask appropriateForURL:nil create:NO error:nil];
+        NSLog(@"file = %@",targetPath);
+        return [documentsDirectoryURL URLByAppendingPathComponent:[response suggestedFilename]];
     } completionHandler:^(NSURLResponse * _Nonnull response, NSURL * _Nullable filePath, NSError * _Nullable error) {
         
         //下载完成调用的方法
         NSLog(@"下载完成：");
         NSLog(@"%@--%@",response,filePath);
+        
+        ZipArchive* zip = [[ZipArchive alloc] init];
+        NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+        NSString *dcoumentpath = ([paths count] > 0) ? [paths objectAtIndex:0] : nil;
+        NSLog(@"%@", dcoumentpath);
+        NSString* l_zipfile = [filePath absoluteString] ;
+        NSString* unzipto = [dcoumentpath stringByAppendingString:@"/test"] ;
+        if( [zip UnzipOpenFile:l_zipfile] ) {
+            NSLog(@"有文件");
+            BOOL ret = [zip UnzipFileTo:unzipto overWrite:YES];
+            if( NO==ret ) {
+                NSLog(@"没解压");
+            }else{
+                NSLog(@"已解压");
+            }
+            [zip UnzipCloseFile];
+        }
+          //[zip release];
+        
+        
     }];
     
     //开始启动任务
